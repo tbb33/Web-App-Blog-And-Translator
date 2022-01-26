7c3f13a9b23eac47203400014b02d3c45e72c7a2
tbb33 <91215510+tbb33@users.noreply.github.com>
2022-01-25 21:54:10 -0600

    Connect template w models with a view
    connected html template blog.html with model (Post class) using view BlogView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+from re import template
 from django.shortcuts import render
 
+from blog.models import Post
+from .models import Post
+
 # Create your views here.
+
+class BlogView:
+    model = Post
+    template_name = 'blog.html'
