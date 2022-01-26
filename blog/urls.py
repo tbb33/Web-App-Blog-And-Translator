ee9dd6bcad241d793765fff4c41f57c80dd31c51
tbb33 <91215510+tbb33@users.noreply.github.com>
2022-01-26 15:15:56 -0600

    Created Home and About page
    Created simple about page to learn process of creating new pgs.
    Created home page to lists all blog posts that have status=publish and order by newest to oldest.
    Learned about ListView=another type of view that renders multiple data rows (here posts)

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     #django looks for slug in tbl then executes BlogView class as view
     path('<slug:slug>', views.BlogView.as_view(), name='blog_view'),
-    path('', views.HomeView.as_view(), name= 'home_view')
+    path('', views.PostList.as_view(), name= 'home'),
+    path('about/', views.AboutView.as_view(), name = 'about_view')
     ]
