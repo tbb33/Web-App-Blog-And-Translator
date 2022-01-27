af122a913e75277f2dd57eb8b03330daf9e006fd
tbb33 <91215510+tbb33@users.noreply.github.com>
2022-01-27 12:23:52 -0600

    Processed user input through a form
    Right now keeping original text in 1st text box and output capitalized version in 2nd box when click submit.
    
    If POST request then get original text through request.POST from my_textarea in html – returns original page translator.html and dict of values of the var output_text w output value and var original_text w original_text value.
    In translator.html use template vars to use those vars that are returned.

--- translator/views.py
+++ translator/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 
 #request param gets url passed by url
 def translator_view(request):
-    return render(request, 'translator.html')
+    if request.method == 'POST':
+        original_text = request.POST['my_textarea']
+        output = original_text.upper()
+        return render(request, 'translator.html', 
+        {'output_text':output, 'original_text':original_text})
+    else:
+        return render(request, 'translator.html')
